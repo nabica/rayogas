161f2c64276dc4d4ed444529c9710d1a2c4920e5
Sergio Aristizabal <shaka-1989@hotmail.com>
2022-01-05 06:39:17 -0500

    avances home 02

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -173,9 +173,12 @@
                 <div class="request-service">
                     <div class="row">
                         <div class="col-md-3">
+                            <div class="relative-container">
                             <div class="image">
                                 <img src="{{ asset('images/web/common/img_formulario_solicitar_servicio.png') }}" class="img-flui" alt="">
                             </div>
+                            </div>
+
                         </div>
                         <div class="col-md-9">
                             <div class="content">
@@ -185,44 +188,103 @@
                                 </div>
 
                                 <form action="">
-                                    <div class="row">
-                                        <div class="col">
-                                            <label for="">Nombre</label>
-                                            <input type="text" class="form-control" placeholder="Nombre" aria-label="Nombre">
+                                    <div class="steps">
+                                        <div class="step ">
+                                            <div class="row">
+                                                <div class="col">
+                                                    <label for="">Nombre</label>
+                                                    <input type="text" class="form-control" placeholder="Nombre" aria-label="Nombre">
+                                                </div>
+                                                <div class="col">
+                                                    <label for="">Número de contacto</label>
+                                                    <input type="text" class="form-control" placeholder="Número de contacto" aria-label="Número de contacto">
+                                                </div>
+                                            </div>
+                                            <div class="row">
+                                                <div class="col">
+                                                    <label for="">Tipo de persona</label>
+                                                    <select name="" id="">
+                                                        <option value="">Tipo de persona</option>
+                                                    </select>
+                                                </div>
+                                                <div class="col">
+                                                    <label for="">Razón social</label>
+                                                    <input type="text" class="form-control" placeholder="Razón social" aria-label="Razón social">
+                                                </div>
+                                            </div>
+                                            <div class="row controls">
+                                                <div class="col-md-1">
+
+                                                </div>
+                                                <div class="col-md-11">
+                                                    <div class="buttons">
+                                                        <button class="btn btn-primary">
+                                                            >
+                                                        </button>
+                                                    </div>
+                                                </div>
+                                            </div>
+
                                         </div>
-                                        <div class="col">
-                                            <label for="">Número de contacto</label>
-                                            <input type="text" class="form-control" placeholder="Número de contacto" aria-label="Número de contacto">
+                                        <div class="step active">
+                                            <div class="row">
+                                                <div class="col">
+                                                    <label for="">Cilindro o granel</label>
+                                                    <select name="" id="">
+                                                        <option value="">Cilindro</option>
+                                                    </select>
+                                                </div>
+                                                <div class="col">
+                                                    <label for="">Cantidad</label>
+                                                    <select name="" id="">
+                                                        <option value="">1 unidad</option>
+                                                    </select>
+                                                </div>
+                                            </div>
+                                            <div class="row">
+                                                <div class="col">
+                                                    <label for="">Presentación / Volumen</label>
+                                                    <select name="" id="">
+                                                        <option value="">40 libras</option>
+                                                    </select>
+                                                </div>
+                                                <div class="col">
+
+                                                </div>
+                                            </div>
+                                            <div class="row controls">
+                                                <div class="col-md-1">
+
+                                                </div>
+                                                <div class="col-md-11">
+                                                    <div class="buttons">
+                                                        {{-- <button class="btn btn-primary">
+                                                            >
+                                                        </button> --}}
+                                                        <a href="" class="btn btn-green">
+                                                            Solicitar
+                                                            <div>Pago en efectivo</div>
+                                                        </a>
+                                                        <a href="" class="btn">
+                                                            Solicitar
+                                                            <div>Pago en línea</div>
+                                                        </a>
+                                                    </div>
+                                                </div>
+                                            </div>
                                         </div>
                                     </div>
-                                    <div class="row">
-                                        <div class="col">
-                                            <label for="">Nombre</label>
-                                            <select name="" id="">
-                                                <option value="">Tipo de persona</option>
-                                            </select>
-                                        </div>
-                                        <div class="col">
-                                            <label for="">Número de contacto</label>
-                                            <input type="text" class="form-control" placeholder="Razón social" aria-label="Razón social">
-                                        </div>
-                                    </div>
-                                    <div class="row buttons">
+
+
+                                    <div class="row indicators">
                                         <div class="col-md-3">
-                                            puntos
+                                            <div class="dots">
+                                                <span class="dots-item active"></span>
+                                                <span class="dots-item "></span>
+                                            </div>
                                         </div>
                                         <div class="col-md-9">
-                                            {{-- <button class="btn btn-primary">
-                                                >
-                                            </button> --}}
-                                            <a href="" class="btn btn-green">
-                                                Solicitar
-                                                <div>Pago en efectivo</div>
-                                            </a>
-                                            <a href="" class="btn">
-                                                Solicitar
-                                                <div>Pago en línea</div>
-                                            </a>
+
                                         </div>
                                     </div>
                                 </form>
