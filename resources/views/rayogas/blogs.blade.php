8a713027fd88bb9f240bd859970d717abf51feb5
scoronadovasco <scoronadovasco@gmail.com>
2025-04-23 21:54:20 -0500

    style: modified css file for blogblade

--- resources/views/rayogas/blogs.blade.php
+++ resources/views/rayogas/blogs.blade.php
@@ -19,9 +19,15 @@
 @endsection
 @section('title', config('app.name') . ' | Blog - ¡Artículos que te interesan sobre el GLP!')
 @section('content')
+<link rel="stylesheet" href="{{ asset('css/rayogas/blogs.css') }}">
 
 
-
+<section class="blog-header text-center py-5">
+    <div class="container">
+        <h1 class="display-4">Lorem commodo dui. <br> volutpat id lacus Ut</h1>
+        <p class="lead">Morbi ultrices quam Quisque vel vel orci quis ex luctus viverra turpis porta.</p>
+    </div>
+</section>
 <section class="blog-list">
     <div class="container">
         <div class="row">
@@ -32,97 +38,19 @@
                     <img src="{{ $blog->card_image }}" class="card-img-top" alt="Imagen del blog">
                     <div class="card-body">
                         <h5 class="card-title">{{ $blog->title }}</h5> {{-- Título del blog --}}
-
+                        <a href="" class="btn btn-primary">Ver más</a>
                     </div>
                 </div>
             </div>
             @endforeach
-
-            {{-- <a href="#" class="col-lg-4 col-md-6 col-12 blog-list__item">
-                <img src="{{asset('images/web/blog/img_blog_preview_1.png')}}" alt="" class="w-100">
-            <div class="blog-list__item-description">
-                <div class="blog-list__item-title">
-                    <h3>Ecopetrol subastará el 90% de GLP que produce en el país</h3>
-                    <hr>
-                </div>
-                <p class="blog-list__item-text">El próximo viernes Ecopetrol asignará, para los siguientes seis
-                    meses, la distribución nacional del volumen total de Gas Licuado de Propano (GLP), que produce
-                    en las refinerías de Cartagena y Barrancabermeja, así como en los campos de Cusiana y Apiay, y
-                    en la planta de Dina.</p>
-            </div>
-            </a>
-            <a href="#" class="col-lg-4 col-md-6 col-12 blog-list__item">
-                <img src="{{asset('images/web/blog/img_blog_preview_2.png')}}" alt="" class="w-100">
-                <div class="blog-list__item-description">
-                    <div class="blog-list__item-title">
-                        <h3>Esfuerzo oficial no alcanzó para cubrir consumo de Gas Licuado de
-                            Propano</h3>
-                        <hr>
-                    </div>
-                    <p class="blog-list__item-text">Aunque la entrada en funcionamiento de Reficar y Termoyopal ayudó a
-                        resolver la crisis por el desabastecimiento de Gas Licuado de Propano (GLP) en el país, aún
-                        existen problemas para que la oferta pueda atender debidamente la creciente demanda de este
-                        combustible.</p>
-                </div>
-            </a>
-            <a href="#" class="col-lg-4 col-md-6 col-12 blog-list__item">
-                <img src="{{asset('images/web/blog/img_blog_preview_3.png')}}" alt="" class="w-100">
-                <div class="blog-list__item-description">
-                    <div class="blog-list__item-title">
-                        <h3>¿Por qué el GLP es una buena alternativa de combustible para el
-                            país?</h3>
-                        <hr>
-                    </div>
-                    <p class="blog-list__item-text">El GLP se ajusta a la realidad económica de los hogares tanto en
-                        costo como en beneficio. Solo basta con ver en el mundo a los vehículos que utilizan gas propano
-                        y que tienen un rendimiento mucho mayor con relación al número de kilómetros que recorren.</p>
-                </div>
-            </a>
-            <a href="#" class="col-lg-4 col-md-6 col-12 blog-list__item">
-                <img src="{{asset('images/web/blog/img_blog_preview_4.png')}}" alt="" class="w-100">
-                <div class="blog-list__item-description">
-                    <div class="blog-list__item-title">
-                        <h3>El GLP es la clave en los procesos de generación</h3>
-                        <hr>
-                    </div>
-                    <p class="blog-list__item-text">Desde enero del 2010 el sector del Gas Licuado de Petróleo (GLP),
-                        más conocido como gas propano está en un proceso de formalización. Así mismo, durante los
-                        últimos seis meses (enero-junio 2016) estuvo en un proceso de regulación para su actividad. </p>
-                </div>
-            </a>
-            <a href="#" class="col-lg-4 col-md-6 col-12 blog-list__item">
-                <img src="{{asset('images/web/blog/img_blog_preview_5.png')}}" alt="" class="w-100">
-                <div class="blog-list__item-description">
-                    <div class="blog-list__item-title">
-                        <h3>Esfuerzo oficial no alcanzó para cubrir consumo de Gas Licuado de
-                            Propano</h3>
-                        <hr>
-                    </div>
-                    <p class="blog-list__item-text">Aunque la entrada en funcionamiento de Reficar y Termoyopal ayudó a
-                        resolver la crisis por el desabastecimiento de Gas Licuado de Propano (GLP) en el país, aún
-                        existen problemas para que la oferta pueda atender debidamente la creciente demanda de este
-                        combustible.</p>
-                </div>
-            </a>
-            <a href="#" class="col-lg-4 col-md-6 col-12 blog-list__item">
-                <img src="{{asset('images/web/blog/img_blog_preview_6.png')}}" alt="" class="w-100">
-                <div class="blog-list__item-description">
-                    <div class="blog-list__item-title">
-                        <h3>¿Por qué el GLP es una buena alternativa de combustible para el
-                            país?</h3>
-                        <hr>
-                    </div>
-                    <p class="blog-list__item-text">El GLP se ajusta a la realidad económica de los hogares tanto en
-                        costo como en beneficio. Solo basta con ver en el mundo a los vehículos que utilizan gas propano
-                        y que tienen un rendimiento mucho mayor con relación al número de kilómetros que recorren.</p>
-                </div>
-            </a> --}}
+            <nav aria-label="Page navigation" class="blog-list__pagination text-center">
+                <button id="load-more" class="btn btn-custom" data-page="2">Ver más</button>
+            </nav>
         </div>
-        <nav aria-label="Page navigation" class="blog-list__pagination">
-            {{ $blogs->links() }}
-        </nav>
+
     </div>
 </section>
+
 
 @endsection
 
