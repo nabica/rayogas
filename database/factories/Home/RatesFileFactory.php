e8fa5a61b034f5b2ea6b084d677b812d86d9430f
scoronadovasco <scoronadovasco@gmail.com>
2025-05-08 22:57:36 -0500

    backend: add files for crud ratesfiles

---
+++ database/factories/Home/RatesFileFactory.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Database\Factories\Home;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class RatesFileFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        return [
+            //
+        ];
+    }
+}
