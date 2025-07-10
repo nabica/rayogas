9e3c3a84279fc40bb60ffa8c279c3dbad6058dd9
scoronado <scoronadovasco@gmail.com>
2025-07-09 22:11:32 -0500

    fix: simplify getFolderId method to return the raw ID

--- app/Models/Blog/Blog.php
+++ app/Models/Blog/Blog.php
@@ -35,7 +35,7 @@ class Blog extends Model
     }
     public function getFolderId()
     {
-        return ($this->id < 10) ? sprintf('%02d', $this->id) : $this->id;
+        return $this->id;
     }
     public function getImageUrlAttribute()
     {
