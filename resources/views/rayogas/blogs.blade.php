82ff289d6bb6b06e91c4ff4c74e1223000906b0e
scoronado <scoronadovasco@gmail.com>
2025-07-09 22:14:45 -0500

    fix: update image upload paths to use 'uploads/blog_posts' instead of 'uploads/blog'

--- resources/views/rayogas/blogs.blade.php
+++ resources/views/rayogas/blogs.blade.php
@@ -32,7 +32,7 @@
         <div class="parent-blog">
             @foreach ($blogs as $blog)
             <div class="card-blog">
-                <img src="{{ asset('uploads/blog/' . $blog->id . '/' . $blog->card_image) }}" class="img-blog" alt="Img del blog">
+                <img src="{{ asset('uploads/blog_posts/' . $blog->id . '/' . $blog->card_image) }}" class="img-blog" alt="Img del blog">
                 <div class="card-body">
                     <h4 class="card-title">{{ $blog->title }}</h4>
                     <a href="{{ route('rayogas.blog.show', $blog->id) }}" class="btn-see-more">Ver más</a>
