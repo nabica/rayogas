bdcab4f45af8e555ddce45d6272a3a7bb763b0b3
scoronado <scoronadovasco@gmail.com>
2025-07-20 16:04:51 -0500

    fix: enhance rates file grouping by year and month, include created_at in grouped data

--- app/Http/Controllers/Rayogas/RatesFileController.php
+++ app/Http/Controllers/Rayogas/RatesFileController.php
@@ -11,8 +11,15 @@ class RatesFileController extends Controller
     public function index()
     {
         $ratesFiles = RatesFile::with('zone')->get();
+        $yearunique = $ratesFiles->pluck('created_at')->map(function ($date) {
+            return \Carbon\Carbon::parse($date)->format('Y');
+        })->unique()->values()->all();
         $allZones = \App\Models\Home\Zone::orderBy('id')->get();
-        $monthsWithFiles = $ratesFiles->pluck('month')->unique()->values()->all();
+        $monthsWithFiles = $ratesFiles->map(function ($file) {
+            $month = $file->month;
+            $year = \Carbon\Carbon::parse($file->created_at)->format('Y');
+            return "{$month} {$year}";
+        })->unique()->values()->all();
         $groupedRates = collect($monthsWithFiles)->mapWithKeys(function ($month) use ($allZones) {
             return [$month => $allZones->map(function ($zone) {
                 return [
@@ -21,13 +28,15 @@ class RatesFileController extends Controller
                     'zone_name' => $zone->name,
                     'description' => null,
                     'zone_id' => $zone->id,
-                    'has_file' => false
+                    'has_file' => false,
+                    'created_at' => null,
                 ];
             })->all()];
         })->all();
 
         foreach ($ratesFiles as $file) {
-            $groupedRates[$file->month] = collect($groupedRates[$file->month])->map(function ($zone) use ($file) {
+            $monthYear = "{$file->month} " . \Carbon\Carbon::parse($file->created_at)->format('Y');
+            $groupedRates[$monthYear] = collect($groupedRates[$monthYear])->map(function ($zone) use ($file) {
                 if ($zone['zone_id'] === $file->zone->id) {
                     return [
                         'id' => $file->id < 10 && $file->id > 0 ? '0' . $file->id : $file->id,
@@ -35,12 +44,24 @@ class RatesFileController extends Controller
                         'zone_name' => $file->zone->name,
                         'description' => $file->description,
                         'zone_id' => $file->zone->id,
-                        'has_file' => true
+                        'has_file' => true,
+                        'created_at' => $file->created_at ? $file->created_at->format('Y-m-d H:i:s') : null,
                     ];
                 }
                 return $zone;
             })->all();
         }
+
+        $groupedRates = collect($groupedRates)->sortByDesc(function ($value, $key) {
+            [$monthName, $year] = explode(' ', $key);
+            $months = [
+                'Enero' => 1, 'Febrero' => 2, 'Marzo' => 3, 'Abril' => 4,
+                'Mayo' => 5, 'Junio' => 6, 'Julio' => 7, 'Agosto' => 8,
+                'Septiembre' => 9, 'Octubre' => 10, 'Noviembre' => 11, 'Diciembre' => 12,
+            ];
+            $monthNumber = $months[$monthName] ?? 0;
+            return ((int)$year * 100) + $monthNumber;
+        })->all();
 
         return view('rayogas.rates', compact('groupedRates'));
     }
