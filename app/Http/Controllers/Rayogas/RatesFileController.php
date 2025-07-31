243b2d0784952af8961074d83fabd2b9192b64b1
scoronadovasco <scoronadovasco@gmail.com>
2025-07-30 21:13:03 -0500

    fix: refactor rates file grouping logic to improve clarity and organization

--- app/Http/Controllers/Rayogas/RatesFileController.php
+++ app/Http/Controllers/Rayogas/RatesFileController.php
@@ -3,66 +3,61 @@
 namespace App\Http\Controllers\Rayogas;
 
 use App\Models\Home\RatesFile;
+use App\Models\Home\Zone;
 use App\Http\Controllers\Controller;
 
 class RatesFileController extends Controller
 {
-
     public function index()
     {
         $ratesFiles = RatesFile::with('zone')->get();
-        $yearunique = $ratesFiles->pluck('created_at')->map(function ($date) {
-            return \Carbon\Carbon::parse($date)->format('Y');
-        })->unique()->values()->all();
-        $allZones = \App\Models\Home\Zone::orderBy('id')->get();
-        $monthsWithFiles = $ratesFiles->map(function ($file) {
-            $month = $file->month;
-            $year = \Carbon\Carbon::parse($file->created_at)->format('Y');
-            return "{$month} {$year}";
-        })->unique()->values()->all();
-        $groupedRates = collect($monthsWithFiles)->mapWithKeys(function ($month) use ($allZones) {
-            return [$month => $allZones->map(function ($zone) {
-                return [
-                    'id' => null,
-                    'file_name' => null,
-                    'zone_name' => $zone->name,
-                    'description' => null,
-                    'zone_id' => $zone->id,
-                    'has_file' => false,
-                    'created_at' => null,
-                ];
-            })->all()];
-        })->all();
+        $allZones = Zone::orderBy('id')->get();
+        $groupedByYear = [];
 
         foreach ($ratesFiles as $file) {
-            $monthYear = "{$file->month} " . \Carbon\Carbon::parse($file->created_at)->format('Y');
-            $groupedRates[$monthYear] = collect($groupedRates[$monthYear])->map(function ($zone) use ($file) {
-                if ($zone['zone_id'] === $file->zone->id) {
+            $year = $file->created_at->format('Y');
+            $month = $file->month;
+
+            if (!isset($groupedByYear[$year][$month])) {
+                $groupedByYear[$year][$month] = $allZones->map(function ($zone) {
                     return [
-                        'id' => $file->id < 10 && $file->id > 0 ? '0' . $file->id : $file->id,
-                        'file_name' => $file->file_name,
-                        'zone_name' => $file->zone->name,
-                        'description' => $file->description,
-                        'zone_id' => $file->zone->id,
-                        'has_file' => true,
-                        'created_at' => $file->created_at ? $file->created_at->format('Y-m-d H:i:s') : null,
+                        'id' => null,
+                        'file_name' => null,
+                        'zone_name' => $zone->name,
+                        'description' => null,
+                        'zone_id' => $zone->id,
+                        'has_file' => false,
+                        'created_at' => null,
                     ];
-                }
-                return $zone;
-            })->all();
+                })->keyBy('zone_id')->all();
+            }
+
+            $groupedByYear[$year][$month][$file->zone->id] = [
+                'id' => $file->id < 10 && $file->id > 0 ? '0' . $file->id : $file->id,
+                'file_name' => $file->file_name,
+                'zone_name' => $file->zone->name,
+                'description' => $file->description,
+                'zone_id' => $file->zone->id,
+                'has_file' => true,
+                'created_at' => $file->created_at ? $file->created_at->format('Y-m-d H:i:s') : null,
+            ];
         }
 
-        $groupedRates = collect($groupedRates)->sortByDesc(function ($value, $key) {
-            [$monthName, $year] = explode(' ', $key);
-            $months = [
-                'Enero' => 1, 'Febrero' => 2, 'Marzo' => 3, 'Abril' => 4,
-                'Mayo' => 5, 'Junio' => 6, 'Julio' => 7, 'Agosto' => 8,
-                'Septiembre' => 9, 'Octubre' => 10, 'Noviembre' => 11, 'Diciembre' => 12,
-            ];
-            $monthNumber = $months[$monthName] ?? 0;
-            return ((int)$year * 100) + $monthNumber;
-        })->all();
+        $monthsOrder = [
+            'Enero', 'Febrero', 'Marzo', 'Abril', 'Mayo', 'Junio',
+            'Julio', 'Agosto', 'Septiembre', 'Octubre', 'Noviembre', 'Diciembre'
+        ];
+        krsort($groupedByYear);
+        foreach ($groupedByYear as &$months) {
+            uksort($months, function ($a, $b) use ($monthsOrder) {
+                return array_search($a, $monthsOrder) <=> array_search($b, $monthsOrder);
+            });
+            // Reindexa zonas para cada mes
+            foreach ($months as &$zones) {
+                $zones = array_values($zones);
+            }
+        }
 
-        return view('rayogas.rates', compact('groupedRates'));
+        return view('rayogas.rates', ['groupedRates' => $groupedByYear]);
     }
 }
