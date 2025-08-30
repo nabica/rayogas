a6733f34c2e929f96d890aa7bb062e8417ee6fc7
scoronadovasco <93351224+scoronadovasco@users.noreply.github.com>
2025-08-30 17:42:42 -0500

    Feature/fixed blogs (#21)
    * fix: update blog card structure for improved link accessibility and styling
    
    * fix: wrap next blog items in anchor tag for improved link structure
    
    * fix: include blog folder in view data for improved blog detail rendering
    
    * fix: update blog card structure for improved accessibility and styling

--- app/Http/Controllers/Rayogas/BlogController.php
+++ app/Http/Controllers/Rayogas/BlogController.php
@@ -8,17 +8,27 @@ use App\Models\Blog\Blog;
 
 class BlogController extends Controller
 {
+    protected $blogfolder;
+
+    public function __construct()
+    {
+        $this->blogfolder = config('rayogas.blog.posts');
+    }
+
     public function index()
     {
-        $blogs =  Blog::latest('id')->paginate(9);
-        return view('rayogas.blogs', compact('blogs'));
+        $blogs = Blog::latest('id')->paginate(9);
+        $blogfolder = $this->blogfolder;
+        return view('rayogas.blogs', compact('blogs', 'blogfolder'));
     }
+
     public function show($id)
     {
         $blog = Blog::findOrFail($id);
         $date = $blog->created_at->locale('es')->isoFormat('DD [de] MMMM [de] YYYY');
         $body_blog = $blog->body_blog;
         $next_blogs = Blog::where('id', '!=', $id)->latest('id')->take(3)->get();
-        return view('rayogas.blogs-detail', compact('blog', 'date', 'next_blogs', 'body_blog'));
+        $blogfolder = $this->blogfolder;
+        return view('rayogas.blogs-detail', compact('blog', 'date', 'next_blogs', 'body_blog', 'blogfolder'));
     }
 }
--- resources/views/rayogas/blogs.blade.php
+++ resources/views/rayogas/blogs.blade.php
@@ -31,15 +31,14 @@
     <section class="blog-list">
         <div class="parent-blog">
             @foreach ($blogs as $blog)
-            <div class="card-blog">
+            <article class="card-blog" onclick="window.location.href='{{ route('rayogas.blog.show', $blog->id) }}'">
                 <img src="{{ asset('uploads/blog_posts/' . $blog->id . '/' . $blog->card_image) }}" class="img-blog" alt="Img del blog">
                 <div class="card-body">
                     <h4 class="card-title">{{ $blog->title }}</h4>
-                    <a href="{{ route('rayogas.blog.show', $blog->id) }}" class="btn-see-more">Ver más</a>
+                    <span class="btn-see-more">Ver más</span>
                 </div>
-            </div>
+            </article>
             @endforeach
-
         </div>
         <div class="load-more-container">
             @if ($blogs->hasMorePages())
